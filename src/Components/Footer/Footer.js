cceab09d41b6d65ca55a5aeef9b5054a377b2552
tanzirrazu <85675153+tanzirrazu@users.noreply.github.com>
2021-10-04 18:18:25 +0600

    clean code

--- src/Components/Footer/Footer.js
+++ src/Components/Footer/Footer.js
@@ -3,33 +3,34 @@ import React from 'react';
 const Footer = () => {
     return (
         < >
-          <footer className=" bg-dark py-3 px-3">
-            <div className="row">
-                <div className="col-md-4 col-lg-4 col-sm-12 text-white">
-                    <h6 className="fw-bold">Useful links</h6>
-                    <div>
-                        <li> <a className="text-decoration-none text-white" href="#Learn">Learn More</a> </li>
-                        <li> <a className="text-decoration-none text-white" href="#Learn">Careers</a> </li>
-                        <li> <a className="text-decoration-none text-white" href="#Learn">FAQs</a> </li>
+        <footer className="bg-dark text-light py-3">
+            <div className="container">
+                <div className="row">
+                    <div className="col-sm-6 col-md-3 item">
+                        <h3>Services</h3>
+                        <ul>
+                            <li><a className="text-decoration-none text-light" href="#biology">Master of Biology</a></li>
+                            <li><a className="text-decoration-none text-light" href="#english">Master of English</a></li>
+                            <li><a className="text-decoration-none text-light" href="#computer">Master of Computer Science</a></li>
+                        </ul>
+                    </div>
+                    <div className="col-sm-6 col-md-3">
+                        <h3>About</h3>
+                        <ul >
+                            <li><a className="text-decoration-none text-light" href="#success">Success Story</a></li>
+                            <li><a className="text-decoration-none text-light" href="#team">Team</a></li>
+                            <li><a className="text-decoration-none text-light" href="#careers">Careers</a></li>
+                        </ul>
+                    </div>
+                    <div className="col-md-6">
+                        <h3>Havard Academy</h3>
+                        <p>Dedicated to increasing our knowledge of the culture, history, and institutions
+                        of the world's major regions and countries.</p>
                     </div>
                 </div>
-
-                <div className="col-md-4 col-lg-4 col-sm-12 text-white mt-5">
-                   <p className="text-lg-center">Copyright &#169; by tanzir-razu</p>
-                </div>
-
-                <div className="col-md-4 col-lg-4 col-sm-12 text-white text-lg-end">
-                    <h6 className="fw-bold">Contact</h6>
-                    <div className="d-block">
-                        <span>Email: tanzirrazu@gmail.com</span>
-                        <br />
-                        <span>Phone: 01995879410</span>
-                        <br />
-                        <span>Tel: 00889463</span>
-                    </div>
-                </div>
+                <p className="copyright">Havard Academy © 2021</p>
             </div>
-          </footer>
+        </footer>
         </>
     );
 };
